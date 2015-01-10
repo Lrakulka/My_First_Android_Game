4b7dd298841a126cd485a7a66ff8a17a1e67b482
Lrakulka <olecsandr32@gmail.com>
2015-01-10 16:31:57 +0200

    Game behavior

--- app/src/main/java/com/example/krabiysok/my_first_android_game/GameProcess.java
+++ app/src/main/java/com/example/krabiysok/my_first_android_game/GameProcess.java
@@ -1,7 +1,13 @@
 package com.example.krabiysok.my_first_android_game;
 
+import android.graphics.Canvas;
 import android.util.Log;
 
+import com.example.krabiysok.my_first_android_game.com.example.krabiysok.sprites.Player;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Vector;
 
 
 /**
@@ -34,10 +40,47 @@ public class GameProcess implements Runnable {
 
     @Override
     public void run() {
+        Canvas canva;
+        Player player = new Player(gameScreen.getWindowSize().x,
+                gameScreen.getWindowSize().y, joystick);
+        ArrayList<Enemie> enemies = new ArrayList<>();
+        ArrayList<Bullet> bullets = new ArrayList<>();
+        ArrayList<Present> presents = new ArrayList<>();
+        Enimie enimie;
+        Present present;
+        Random random = new Random();
         while (!stop) {
-            Log.d("LogApp", "as");
+            Log.d("LogApp", "game Go");
+            if (enemies.isEmpty() || random.nextInt() % 20 < Player.getScore100())
+                switch (random.nextGaussian() % 3) {
+                    case 0: { enemies.add(new Enimie()); break;}
+                    case 1: { enemies.add(new Enimie()); break;}
+                    case 2: { enemies.add(new Enimie()); break;}
+                }
+            canva = gameScreen.getCanvas();
+            bullets.addAll(player.action(canva));
+            presents.addAll(mrCat.action(canva));
+            for(int i = 0; i < enemies.size(); ++i) {
+                enimie = enemies.get(i);
+                bullets.addAll(enimie.action(canva));
+                if (!enimie.isAlive(bullets)) {
+                    enemies.remove(i);
+                    i--;
+                }
+            }
+            for(int i = 0; i < presents.size(); ++i) {
+                present = presents.get(i);
+                if (present.takes(player))
+                    i--;
+                else presents.draw(canva);
+            }
+            if (!player.isAlive(bullets))
+                stopGame(); //---
+            for(Bullet bullet : bullets)
+                bullet.draw(canva);
+            gameScreen.draw(canva);
             try {
-                Thread.sleep(2000);
+                Thread.sleep(100);
                 if (sleepGame)
                     synchronized (gameThread) {
                         gameThread.wait();
---
+++ app/src/main/java/com/example/krabiysok/my_first_android_game/com/example/krabiysok/sprites/Player.java
@@ -0,0 +1,12 @@
+package com.example.krabiysok.my_first_android_game.com.example.krabiysok.sprites;
+
+import android.app.Activity;
+
+/**
+ * Created by KrabiySok on 1/10/2015.
+ */
+public class Player extends GeneralAnimation {
+    Player(Activity activity, int rows, int columns, int widthAnimRezol, int heightAnimRezol, int sprite) {
+        super(activity, rows, columns, widthAnimRezol, heightAnimRezol, sprite);
+    }
+}
